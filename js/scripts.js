52c1d6bd59f33d37514402d42d677fedb889e3cc
Shahid Akhtar Khan <63913889+shahidedu7@users.noreply.github.com>
2025-01-03 01:28:34 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -22,7 +22,6 @@ function renderArticles(articles, page) {
 
     paginatedArticles.forEach(article => {
         articlesContainer.innerHTML += `
-            <div class="col-md-6 mb-4">
                 <div class="card post-card mb-4">
                     <div class="card-body">
                         <h5 class="card-title">${article.title}</h5>
@@ -30,7 +29,6 @@ function renderArticles(articles, page) {
                         <a href="${article.url}" class="btn btn-primary">Read More</a>
                     </div>
                 </div>
-            </div>
         `;
     });
 }
